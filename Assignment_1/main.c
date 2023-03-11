0e55de3428e41d38c5a819943bc8679fef11f28f
Yash.K <97033051+yxsh7@users.noreply.github.com>
2023-03-11 14:07:43 +0530

    Add files via upload

---
+++ Assignment_1/main.c
@@ -0,0 +1,19 @@
+// SI32 Yash Kamthe
+
+#include <xc.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<pic18f4550.h>
+
+void main(void)
+{
+    int a,b,sum;
+    a=31;
+    b=31;
+    sum=0;
+    TRISD=0;
+    
+    sum = a+b;
+    
+    PORTD=sum;
+}
