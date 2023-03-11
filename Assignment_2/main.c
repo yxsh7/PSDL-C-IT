02087a22ee8a1beaec998e7c6f5e6208fb56a86c
Yash.K <97033051+yxsh7@users.noreply.github.com>
2023-03-11 14:25:49 +0530

    Add files via upload

---
+++ Assignment_2/main.c
@@ -0,0 +1,21 @@
+// SI32 Yash Kamthe
+
+#include <pic18f4550.h>
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+    int i,sum;
+    sum = 0;
+    int number[] = {1,2,3,4,5,6,7};
+    
+    for(i=0;i<=6;i++)
+    {
+        sum = sum + number[i];
+    }
+    
+    TRISD = 0;
+    PORTD = sum;
+}
