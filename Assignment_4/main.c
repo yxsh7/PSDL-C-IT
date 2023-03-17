25b3929d9327dcfce381076e7a09d15122cc1215
Yash.K <97033051+yxsh7@users.noreply.github.com>
2023-03-17 13:22:01 +0530

    Add files via upload

---
+++ Assignment_4/main.c
@@ -0,0 +1,24 @@
+// SI32 Yash Kamthe
+
+#include <xc.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<pic18f4550.h>
+
+void main(void)
+{
+    int multiplication_result, division_result, num_1, num_2;
+    multiplication_result = 0;
+    division_result = 0;
+    num_1 = 0x01;
+    num_2 = 0x02;
+    
+    multiplication_result = num_1 * num_2;
+    division_result = num_2 / num_1;
+    
+    TRISC = 0;
+    PORTC = multiplication_result; 
+    
+    TRISD = 0; 
+    PORTD = division_result;
+}
